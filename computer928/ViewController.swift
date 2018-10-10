d1aa9007adeb8ba66d49e11851bda2b07664cefe
20161104583 <dorgen@CIEC-iMacs-iMac.local>
2018-10-10 10:15:17 +0800

    computer928
    Computer928

--- computer928/ViewController.swift
+++ computer928/ViewController.swift
@@ -10,6 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var coma: UITextField!
+    
+    @IBAction func comb(_ sender: Any) {
+    }
+    
+    @IBAction func comc(_ sender: Any) {
+    }
+    
+    @IBAction func comd(_ sender: Any) {
+    }
+    
+    @IBAction func come(_ sender: Any) {
+    }
+    
+    @IBAction func comf(_ sender: Any) {
+    }
+    
+    @IBAction func comg(_ sender: Any) {
+    }
+    
+    @IBAction func comh(_ sender: Any) {
+    }
+    
+    @IBAction func comi(_ sender: Any) {
+    }
+    
+    
+    @IBAction func comm(_ sender: Any) {
+    }
+    
+    @IBAction func comj(_ sender: Any) {
+    }
+    
+    @IBAction func coml(_ sender: Any) {
+    }
+    
+    @IBAction func comn(_ sender: Any) {
+    }
+    
+    @IBAction func como(_ sender: Any) {
+    }
+    
+    @IBAction func comp(_ sender: Any) {
+    }
+    
+    @IBAction func comq(_ sender: Any) {
+    }
+    
+    @IBAction func comx(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
